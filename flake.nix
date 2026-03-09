bcf0617010247ba5599c7bd907ed08e1f37db85e
sacolle <pedro.h.b.colle@gmail.com>
2026-03-09 15:07:08 -0300

    modificado o flake para melhor de adequar ao novo proposito do repo. Deve-se mudar a compilação do StarPU, principalmente com a flag --max-cpu...

--- flake.nix
+++ flake.nix
@@ -1,56 +1,53 @@
 {
-    description = "A very basic flake";
+    description = "StarPU derivation for NixOS.";
 
     inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
         # There is a bug involving glibc and NVCC. 
         # using this specific commti hash, that references glibc 2.40-36
         # makes it work
-        nixpkgs.url = "github:nixos/nixpkgs/1da52dd49a127ad74486b135898da2cef8c62665";
+        cudaNixpkgs.url = "github:nixos/nixpkgs/1da52dd49a127ad74486b135898da2cef8c62665";
     };
 
-    outputs = { self, nixpkgs }: 
+    outputs = { self, nixpkgs, cudaNixpkgs }: 
     let 
         system = "x86_64-linux";
-        starpuOverlay = f: p: {
-            StarPU = p.callPackage ./starpu.nix { enableCUDA = true; };
-        };
-        fxtOverlay = f: p: {
-            fxt = p.callPackage ./fxt.nix { static = true; };
-        };
-        pkgs = import nixpkgs {
-            inherit system;
-            overlays = [ starpuOverlay fxtOverlay ];
+
+        cudapkgs = import cudaNixpkgs { 
+            inherit system; 
             config = { 
                 allowUnfree = true;
                 cudaSupport = true;
                 cudaVersion = "13";
             };
         };
+        pkgs = import nixpkgs {
+            inherit system;
+            config = { 
+                allowUnfree = true;
+                cudaSupport = true;
+                cudaVersion = "13";
+            };
+        };
+
+        fxt = pkgs.callPackage ./fxt.nix { static = true; };
+        StarPU = pkgs.callPackage ./starpu.nix { 
+            # enableCUDA = true; 
+            # use the proper cuda pkgs for no compilation error 
+            cudaPackages  = cudapkgs.cudaPackages;
+            linuxPackages = cudapkgs.linuxPackages;
+
+            # pass fxt to be able to implement trace 
+            inherit fxt;
+
+            #maxBuffers = 56;
+            #enableTrace = true;
+        };
     in
     {
-        packages.${system}.default = pkgs.StarPU;
-        devShells.${system}.default = pkgs.mkShell {
-            buildInputs = with pkgs; [
-                pkg-config
-                StarPU
-                hwloc
-                # for bash to work properlly inside vscode
-                bashInteractive
-                gdb
-                gcc
-                valgrind
-            ];
-            # export StarPU and hwloc store locations 
-            # for use in vscode intellisence
-            GCC_STORE_PATH = "${pkgs.gcc}";
-            STARPU_STORE_PATH = "${pkgs.StarPU}";
-            HWLOC_STORE_PATH = "${pkgs.hwloc.dev}";
-
-            shellHook = ''
-                export SHELL=/run/current-system/sw/bin/bash
-                echo Added StarPU, Hwloc and gcc to ENV
-                echo ${pkgs.fxt}
-            '';
+        packages.${system} = {
+            default = StarPU;
+            inherit fxt StarPU;
         };
     };
 }
